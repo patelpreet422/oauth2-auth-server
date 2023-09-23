2ce79ded4f46892d944950326e8cf131624c31c1
Preet Patel <patelpreet422@gmail.com>
2023-09-23 10:01:15 +0530

    add some documentation links

--- src/main/java/com/example/authserver/AuthServerApplication.java
+++ src/main/java/com/example/authserver/AuthServerApplication.java
@@ -3,6 +3,11 @@ package com.example.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ Spring authorization server doc: https://docs.spring.io/spring-authorization-server/reference/index.html
+ What is PKCE? https://www.youtube.com/watch?v=h_1JAh3JPkI
+ Why we need state in authorization request?
+ */
 @SpringBootApplication
 public class AuthServerApplication {
 
